4f84edd329c9a8ed87ad7055380f3a3468b0db8a
Tim <82809793+timasshoff@users.noreply.github.com>
2024-12-11 13:43:41 +0100

    Initial Commit

---
+++ src/client/java/com/skytendo/pulseclick/PulseClickClient.java
@@ -0,0 +1,10 @@
+package com.skytendo.pulseclick;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PulseClickClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
