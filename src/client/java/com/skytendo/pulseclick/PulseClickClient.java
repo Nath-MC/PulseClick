a2e7bbc16ae54bebe99733fd2b8c502eb4a4ce94
Tim <82809793+timasshoff@users.noreply.github.com>
2024-12-13 14:58:51 +0100

    Added new features

--- src/client/java/com/skytendo/pulseclick/PulseClickClient.java
+++ src/client/java/com/skytendo/pulseclick/PulseClickClient.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.MessageScreen;
 import net.minecraft.client.gui.screen.TitleScreen;
@@ -56,47 +57,53 @@ public class PulseClickClient implements ClientModInitializer {
 			}
 		}));
 
-		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			if (client.player == null) return; // If we do not have a client, we are not in-game and we want to return.
+		ClientTickEvents.END_CLIENT_TICK.register(this::tick);
+	}
 
-			// Open config if the key was pressed
-			while (configKeyBinding.wasPressed()) {
-				client.setScreen(MidnightConfig.getScreen(client.currentScreen, MOD_ID));
+	private void tick(MinecraftClient client) {
+		// If we do not have a client, we are not in-game and we want to return.
+		if (client.player == null) return;
+
+		// Open config if the key was pressed
+		while (configKeyBinding.wasPressed()) {
+			client.setScreen(MidnightConfig.getScreen(client.currentScreen, MOD_ID));
+		}
+
+		while (activateKeyBinding.wasPressed()) {
+			toggleAutoClicker(client);
+		}
+
+		if (isClicking) {
+			// Release the key that might have been pressed in the previous tick
+			releaseKey(client);
+
+			switch (PulseClickConfig.timingMode) {
+				case TICKS -> tickMode(client);
+				case ATTACK_COOLDOWN -> attackCooldownMode(client);
 			}
 
-			while (activateKeyBinding.wasPressed()) {
-				toggleAutoClicker(client);
-			}
-
-			if (isClicking) {
-				// Release the key that might have been pressed in the previous tick
-				releaseKey(client);
-
-				switch (PulseClickConfig.timingMode) {
-					case TICKS -> {
-						tickMode(client);
-					}
-					case ATTACK_COOLDOWN -> {
-						attackCooldownMode(client);
-					}
-				}
-
-				// Deactivate PulseClick if the player is starving (if enabled)
-				if (PulseClickConfig.preventStarvation) {
-					if (client.player.getHungerManager().getFoodLevel() < 4) {
-						client.player.sendMessage(Text.translatable("pulseclick.message.starvation"), false);
-						toggleAutoClicker(client);
-					}
-				}
-
-				// Disconnect if the player has low health (if enabled)
-				if (PulseClickConfig.disconnectOnLowHealth) {
-					if (client.player.getHealth() < 3) {
-						disconnect(client);
-					}
+			// Deactivate PulseClick if the player is starving (if enabled)
+			if (PulseClickConfig.preventStarvation) {
+				if (client.player.getHungerManager().getFoodLevel() < 4) {
+					client.player.sendMessage(Text.translatable("pulseclick.message.starvation"), false);
+					toggleAutoClicker(client);
 				}
 			}
-		});
+
+			if (PulseClickConfig.preventToolBreak) {
+				if (client.player.getMainHandStack().getMaxDamage() - client.player.getMainHandStack().getDamage() < 10) {
+					client.player.sendMessage(Text.translatable("pulseclick.message.toolbreak"), false);
+					toggleAutoClicker(client);
+				}
+			}
+
+			// Disconnect if the player has low health (if enabled)
+			if (PulseClickConfig.disconnectOnLowHealth) {
+				if (client.player.getHealth() < 3) {
+					disconnect(client);
+				}
+			}
+		}
 	}
 
 	/**
@@ -105,8 +112,12 @@ public class PulseClickClient implements ClientModInitializer {
 	 */
 	private void toggleAutoClicker(MinecraftClient client) {
 		isClicking = !isClicking;
+		tickCounter = 0;
 		if (isClicking) {
 			client.player.sendMessage(Text.translatable("pulseclick.message.activated"), true);
+			if (PulseClickConfig.timingMode == TimingMode.TICKS) {
+				tickCounter = PulseClickConfig.ticksBetweenClicks;
+			}
 		}
 		else {
 			client.player.sendMessage(Text.translatable("pulseclick.message.deactivated"), true);
--- src/client/java/com/skytendo/pulseclick/PulseClickConfig.java
+++ src/client/java/com/skytendo/pulseclick/PulseClickConfig.java
@@ -13,7 +13,7 @@ public class PulseClickConfig extends MidnightConfig {
     @Comment public static Comment spacer2;
 
     @Comment public static Comment tickBetweenClicksComment;
-    @Entry(min = 5, max = 10000) public static int ticksBetweenClicks = 100;
+    @Entry(min = 5, max = 10000) public static int ticksBetweenClicks = 10;
 
     @Comment public static Comment spacer3;
 
@@ -21,6 +21,11 @@ public class PulseClickConfig extends MidnightConfig {
     @Entry() public static boolean preventStarvation = true;
 
     @Comment public static Comment spacer4;
+
+    @Comment public static Comment preventToolBreakComment;
+    @Entry() public static boolean preventToolBreak = true;
+
+    @Comment public static Comment spacer5;
 
     @Comment public static Comment disconnectOnLowHealthComment;
     @Entry() public static boolean disconnectOnLowHealth = false;
